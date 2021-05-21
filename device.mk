2dc100908cb44a15da12e85617192ee20e6e2b7e
LuK1337 <priv.luk@gmail.com>
2021-05-21 15:32:47 +0200

    thyme: Set default Settings.Global.DEVICE_NAME according to MIUI
    MIUI uses ro.product.vendor.marketname for default device name.
    
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += NfcTargetOverlay
 PRODUCT_PACKAGES += \
     ApertureTargetOverlay \
     FrameworksTargetOverlay \
+    SettingsProviderTargetOverlay \
     SettingsTargetOverlay \
     SettingsTargetRegInfoOverlay \
     SystemUITargetOverlay
